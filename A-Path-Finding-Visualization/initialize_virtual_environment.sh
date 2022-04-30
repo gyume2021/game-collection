e79e5543d8ba29373a483cc8faee24c09f53336b
gyume2021 <letsencrypthuang@gmail.com>
2022-04-30 12:07:37 +0800

    add a star path finding example

---
+++ A-Path-Finding-Visualization/initialize_virtual_environment.sh
@@ -0,0 +1,7 @@
+virtualenv -p /usr/bin/python A-star-pathfinder
+source A-star-pathfinder/bin/activate
+deactivate
+pip list # list installed package
+
+pip3 install pygame
+pip3 freeze > requirements.txt # export requirement
